39a5ac400641349c70ed5992a0488b223b9ca289
SonPhatTranDeveloper <phat.tran.developer@gmail.com>
2025-09-10 11:43:42 +0700

    Add word-level transformation

---
+++ src/transformation/word_level.py
@@ -0,0 +1,24 @@
+from src.transformation.base import TransformationBase
+
+from textattack.augmentation.recipes import CLAREAugmenter
+
+
+class CLARETransformation(TransformationBase):
+    """
+    Transformation that operates at the word level.
+
+    References:
+        - Li, Zhang, Peng, Chen, Brockett, Sun, Dolan.
+        - “Contextualized Perturbation for Textual Adversarial Attack” (Li et al., 2020)
+        - https://arxiv.org/abs/2009.07502
+
+    CLARE builds on a pre-trained masked language model and modifies the inputs in a contextaware manner.
+    We propose three contextualized perturbations, Replace, Insert and Merge,
+    allowing for generating outputs of varied lengths.
+    """
+
+    def __init__(self):
+        self.augmenter = CLAREAugmenter()
+
+    def transform(self, sentence: str) -> str:
+        return self.augmenter.augment(sentence)
