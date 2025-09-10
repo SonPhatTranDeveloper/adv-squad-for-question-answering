3f4deb174c34c7f33eb6fbf0d69db7a595adb799
SonPhatTranDeveloper <phat.tran.developer@gmail.com>
2025-09-10 10:23:55 +0700

    Create initial project

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from perturbed-squad-question-answering!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+"""Perturbed Squad Question Answering package."""
